49318f839b8aa8ed520296cbf46d27134f495b8b
steve austin <austines811@gmail.com>
2022-06-08 10:01:42 +0300

    commit tree 36818d7fd4b4ea7b90ad782566a6c25a856fac39

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Code your solution in this file!
+//Write your code here
 function distanceFromHqInBlocks(start){
     const schuberHq = 42;
     if (start > schuberHq){
         return (start - schuberHq);
     }else{
-        return  (schuberHq - start);
+        return (schuberHq - start);
     }
 }
 function distanceFromHqInFeet(start){
@@ -14,19 +14,19 @@ function distanceTravelledInFeet(start, destination){
     if (start > destination){
         var distance = start - destination;
     }else{
-        var distance = destination - start;
+        var distance= destination - start;
     }
-    return (parseInt((distance), 10) * 264);
+    return (parseInt(distance, 10) * 264)
 }
 function calculatesFarePrice(start, destination){
-    let chargableDist = parseInt((distanceTravelledInFeet(start, destination)));
+    let chargableDist = (parseInt((distanceTravelledInFeet(start, destination)), 10))
     if (chargableDist <= 400){
         return 0;
     }else if (chargableDist <= 2000){
-    return ((chargableDist - 400) * 0.02);
+        return ((chargableDist - 400) * 0.02)
     }else if (chargableDist <= 2500){
         return 25;
-        }else{
-            return "cannot travel that far"
-        }
+    }else{
+        return 'cannot travel that far';
+    }
 }
